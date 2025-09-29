b03a1adaf5ece71110d67847d148428ad86af0a5
Davi Pereira <145718575+Daviidvl@users.noreply.github.com>
2025-09-29 15:49:22 -0300

    novo

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
 import Cliente from "./pages/Cliente";
 import Tecnico from "./pages/Tecnico";
 import NotFound from "./pages/NotFound";
@@ -17,7 +16,6 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
           <Route path="/cliente" element={<Cliente />} />
           <Route path="/tecnico" element={<Tecnico />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
